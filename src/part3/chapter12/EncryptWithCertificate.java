026fef783a813541153a2d99e9161115243802d7
Bruno Lowagie <bruno.lowagie@itextpdf.com>
2010-01-23 11:53:51 +0000

    Reducing two examples to one.

--- src/part3/chapter12/EncryptWithCertificate.java
+++ src/part3/chapter12/EncryptWithCertificate.java
@@ -3,6 +3,7 @@ package part3.chapter12;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.security.GeneralSecurityException;
 import java.security.KeyStore;
 import java.security.KeyStoreException;
 import java.security.NoSuchAlgorithmException;
@@ -25,41 +26,30 @@ import com.itextpdf.text.pdf.PdfReader;
 import com.itextpdf.text.pdf.PdfStamper;
 import com.itextpdf.text.pdf.PdfWriter;
 
-public class EncryptWithCertificate2 {
+public class EncryptWithCertificate {
 
-	public static String RESULT1 = "results/part3/chapter12/certificate_encryption2.pdf";
-	public static String RESULT2 = "results/part3/chapter12/certificate_decrypted2.pdf";
-	public static String RESULT3 = "results/part3/chapter12/certificate_encrypted2.pdf";
+	public static String RESULT1 = "results/part3/chapter12/certificate_encryption.pdf";
+	public static String RESULT2 = "results/part3/chapter12/certificate_decrypted.pdf";
+	public static String RESULT3 = "results/part3/chapter12/certificate_encrypted.pdf";
 	
 	public static String PATH = "c:/home/blowagie/key.properties";
-	
 	public static Properties properties = new Properties();
 	
-	public Certificate getPublicCertificate() throws IOException, CertificateException {
-		FileInputStream is = new FileInputStream(properties.getProperty("PUBLIC"));
+	public Certificate getPublicCertificate(String path) throws IOException, CertificateException {
+		FileInputStream is = new FileInputStream(path);
 		CertificateFactory cf = CertificateFactory.getInstance("X.509");
     	X509Certificate cert = (X509Certificate) cf.generateCertificate(is);
     	return cert;
 	}
 	
-	public PrivateKey getPrivateKey() throws KeyStoreException, NoSuchProviderException, NoSuchAlgorithmException, CertificateException, IOException, UnrecoverableKeyException {
-		String path = properties.getProperty("PRIVATE");
-		String keystore_password = properties.getProperty("PASSWORD");
-		String key_password = properties.getProperty("PASSWORD");
-		KeyStore ks = KeyStore.getInstance("pkcs12", "BC");
-		ks.load(new FileInputStream(path), keystore_password.toCharArray());
-		String alias = (String)ks.aliases().nextElement();
-		PrivateKey pk = (PrivateKey)ks.getKey(alias, key_password.toCharArray());
-		return pk;
-	}
-	
-	public void createPdf(String filename) throws IOException, DocumentException, KeyStoreException, NoSuchProviderException, NoSuchAlgorithmException, CertificateException, UnrecoverableKeyException {
+	public void createPdf(String filename) throws IOException, DocumentException, GeneralSecurityException {
         // step 1
         Document document = new Document();
         // step 2
         PdfWriter writer = PdfWriter.getInstance(document, new FileOutputStream(RESULT1));
-        Certificate cert = getPublicCertificate();
-        writer.setEncryption(new Certificate[]{cert}, new int[]{PdfWriter.ALLOW_PRINTING}, PdfWriter.ENCRYPTION_AES_128);
+        Certificate cert1 = getPublicCertificate("resources/encryption/foobar.cer");
+        Certificate cert2 = getPublicCertificate(properties.getProperty("PUBLIC"));
+        writer.setEncryption(new Certificate[]{cert1, cert2}, new int[]{PdfWriter.ALLOW_PRINTING, PdfWriter.ALLOW_COPY}, PdfWriter.ENCRYPTION_AES_128);
         // step 3
         document.open();
         // step 4
@@ -68,8 +58,18 @@ public class EncryptWithCertificate2 {
         document.close();
 	}
 	
-	public void decryptPdf(String src, String dest) throws IOException, DocumentException, CertificateException, KeyStoreException, NoSuchProviderException, NoSuchAlgorithmException, UnrecoverableKeyException {
-		PdfReader reader = new PdfReader(src, getPublicCertificate(), getPrivateKey(), "BC");
+	public PrivateKey getPrivateKey() throws GeneralSecurityException, IOException {
+		String path = "resources/encryption/.keystore";
+		String keystore_password = "f00b4r";
+		String key_password = "f1lmf3st";
+		KeyStore ks = KeyStore.getInstance(KeyStore.getDefaultType());
+		ks.load(new FileInputStream(path), keystore_password.toCharArray());
+		PrivateKey pk = (PrivateKey)ks.getKey("foobar", key_password.toCharArray());
+		return pk;
+	}
+	
+	public void decryptPdf(String src, String dest) throws IOException, DocumentException, GeneralSecurityException {
+		PdfReader reader = new PdfReader(src, getPublicCertificate("resources/encryption/foobar.cer"), getPrivateKey(), "BC");
 		PdfStamper stamper = new PdfStamper(reader, new FileOutputStream(dest));
 		stamper.close();
 	}
@@ -77,15 +77,15 @@ public class EncryptWithCertificate2 {
 	public void encryptPdf(String src, String dest) throws IOException, DocumentException, CertificateException {
 		PdfReader reader = new PdfReader(src);
 		PdfStamper stamper = new PdfStamper(reader, new FileOutputStream(dest));
-		Certificate cert = getPublicCertificate();
+		Certificate cert = getPublicCertificate("resources/encryption/foobar.cer");
 		stamper.setEncryption(new Certificate[]{cert}, new int[]{PdfWriter.ALLOW_PRINTING}, PdfWriter.ENCRYPTION_AES_128);
 		stamper.close();
 	}
 	
-	public static void main(String[] args) throws IOException, DocumentException, KeyStoreException, NoSuchProviderException, NoSuchAlgorithmException, CertificateException, UnrecoverableKeyException {
+	public static void main(String[] args) throws IOException, DocumentException, GeneralSecurityException {
 		Security.addProvider(new BouncyCastleProvider());
 		properties.load(new FileInputStream(PATH));
-		EncryptWithCertificate2 hello = new EncryptWithCertificate2();
+		EncryptWithCertificate hello = new EncryptWithCertificate();
 		hello.createPdf(RESULT1);
 		hello.decryptPdf(RESULT1, RESULT2);
 		hello.encryptPdf(RESULT2, RESULT3);
