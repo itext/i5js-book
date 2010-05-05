d0dd0a0aa68a75861d37b9ed82cff8eac12c3bed
Bruno Lowagie <bruno.lowagie@itextpdf.com>
2010-05-05 15:26:18 +0000

    Added missing headers.

--- src/part3/chapter10/ImageTypes.java
+++ src/part3/chapter10/ImageTypes.java
@@ -1,5 +1,5 @@
 /*
-<b><i>Submitting a PDF form as HTML POST</i></b> * This class is part of the book "iText in Action - 2nd Edition"
+ * This class is part of the book "iText in Action - 2nd Edition"
  * written by Bruno Lowagie (ISBN: 9781935182610)
  * For more info, go to: http://itextpdf.com/examples/
  * This example only works with the AGPL version of iText.
