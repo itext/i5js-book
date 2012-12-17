f9bcfe1b24eadd521b65353acbb61386062b2911
Bruno Lowagie <bruno.lowagie@itextpdf.com>
2012-12-17 13:44:43 +0000

    Reset of original example after fix by Kevin Day (stream closed by stamper).

--- src/part2/chapter06/SelectPages.java
+++ src/part2/chapter06/SelectPages.java
@@ -36,8 +36,6 @@ public class SelectPages {
         PdfReader reader = new PdfReader(MovieTemplates.RESULT);
         reader.selectPages("4-8");
         manipulateWithStamper(reader);
-        //reader = new PdfReader(MovieTemplates.RESULT);
-        //reader.selectPages("4-8");
         manipulateWithCopy(reader);
     }
 
